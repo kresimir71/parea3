146a0cc8617952ebe065610e80268c42bb3c25d0
kresimir71 <kresjemir@kresjemir-PC.(none)>
2015-06-12 09:52:46 +0200

    added pyindent3parea

---
+++ pyindent3parea/decorate.cpp
@@ -0,0 +1,145 @@
+ /*decorate python program with comment-braces */ 
+
+#define RM2PAREA_LIBRARY_HEADER
+
+#include <stdlib.h>
+#include "pyscan3parea/python.h"
+#include "pyscan3parea/x_rm1.tab.h"
+#include <iostream>
+#include <list>
+
+class TokenRec{
+public:
+  std::string text;
+  int id;
+};
+
+std::list<TokenRec> TokenList;
+int NumberOfDedents=0; /*number of DEDENTS in the current burst, initially zero */
+  
+static void printAllCleanStack(){
+  std::list<TokenRec>::iterator iii=TokenList.begin();
+  for ( ; iii!=TokenList.end();iii++){
+     std::cout << iii->text;
+  }
+  TokenList.clear();
+}
+
+static std::list<TokenRec>::reverse_iterator searchBackToken(int id){
+  std::list<TokenRec>::reverse_iterator iii=TokenList.rbegin();
+  for ( ; iii!=TokenList.rend();iii++){
+    if (id==iii->id){
+      break;
+    }
+  }
+  return iii;
+}
+
+void insertTokenRec(std::list<TokenRec>::reverse_iterator succ, int id, std::string txt){
+  TokenRec temp;
+  temp.id=id;
+  temp.text=txt;
+  TokenList.insert(succ.base(),temp);
+}
+
+bool correctBracesContruct(std::string &txt){
+  for (std::string::iterator iii = txt.begin();iii!=txt.end();iii++){
+    if (iii == txt.begin() && *iii !='#'){
+      break;
+    }
+    if (iii == txt.begin() && *iii =='#'){
+      continue;
+    }
+    if (*iii == '}'){
+      continue;
+    }
+    if (iii-1 == txt.begin() && *iii =='#'){
+      break;
+    } else if (*iii =='#'){
+      std::string tmp = std::string("#")+std::string(NumberOfDedents,'}');
+      txt.replace(txt.begin(),iii,tmp);
+      return true;
+    }
+    break;
+  }
+  return false;
+}
+static int handle_token(){
+  std::list<TokenRec>::reverse_iterator iii;
+  iii=TokenList.rbegin();
+  std::list<TokenRec>::reverse_iterator TokNL;
+  std::list<TokenRec>::reverse_iterator TokBeforeNL;
+
+  if (TOK_INDENT==iii->id){
+    TokNL = searchBackToken(TOK_NEWLINE);
+    if (TokNL==TokenList.rend()){
+      //fatal error, misplaced NL, Before TOK_NEWLINE there has to be some tokens, because TOK_INDENT
+      return 1;
+    }
+    TokBeforeNL=TokNL;
+    TokBeforeNL++;
+    if (TokBeforeNL==TokenList.rend()){
+      //fatal error, misplaced NL, Before any TOK_NEWLINE there has to be some tokens, because TOK_INDENT
+      return 1;
+    }
+    
+    if(TOK_COMMENT!=TokBeforeNL->id){
+      //insert token between TokBeforeNL and TokNL with id=TOK_COMMENT, text = " #{#"
+      insertTokenRec(TokNL,TOK_COMMENT," #{#");
+    }else{
+      if(std::string(TokBeforeNL->text,0,3)=="#{#"){
+	//already there, nothing todo
+      }else{
+	//prepend TokBeforeNL.text with "#{"
+	TokBeforeNL->text=std::string("#{")+TokBeforeNL->text;
+      }
+    }
+    //printAllCleanStack();
+  }else if(TOK_DEDENT==iii->id){
+    NumberOfDedents++;
+  }else{
+    // no indent, no dedent
+    if (NumberOfDedents){
+      TokNL = searchBackToken(TOK_NEWLINE);
+      if (TokNL==TokenList.rend()){
+	//fatal error, misplaced NL, Before any TOK_NEWLINE there has to be some tokens
+	return 1;
+      }
+      TokBeforeNL=TokNL;
+      TokBeforeNL++;
+      if(TOK_COMMENT!=TokBeforeNL->id){
+	//insert token between TokBeforeNL and TokNL with id=TOK_COMMENT, text = "#}...}#", number of braces equals NumberOfDedents
+	insertTokenRec(TokNL,TOK_COMMENT,std::string(" #") + std::string(NumberOfDedents,'}') + std::string("#"));
+      }else{
+	if(correctBracesContruct(TokBeforeNL->text)){ // any positive number of braces in "#}...}#"
+	  //replace with "#}...}#", number of braces equals NumberOfDedents (already done in function)
+	}else{
+	  //prepend TokBeforeNL.text with "#}...}", number of braces equals NumberOfDedents
+	  TokBeforeNL->text=std::string("#")+std::string(NumberOfDedents,'}')+TokBeforeNL->text;
+
+	}
+      }
+      //printAllCleanStack();
+    }
+    NumberOfDedents=0;
+  }
+  return 0;
+}
+
+void scan_callback(long tok_id, char* tokentext, int tokentextlen){
+  
+  TokenRec NewTok;
+  NewTok.id=tok_id;
+  NewTok.text=std::string(tokentext,tokentextlen);
+  TokenList.push_back(NewTok);
+  //  std::cout << tokentext;
+  if (handle_token()){
+    std::cerr << "fatal error";
+    exit (1);
+  }
+  if (tok_id==TOK_EXIT){
+    printAllCleanStack();
+    //std::cerr << "ready";
+  }
+}
+
---
+++ pyindent3parea/main.cpp
@@ -0,0 +1,61 @@
+
+#define RM2PAREA_LIBRARY_HEADER
+
+#include "pyscan3parea/python.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//#include "decorate.h"
+extern void scan_callback(long tok_id, char* tokentext, int tokentextlen);
+
+int main( int argc,  char **argv){
+
+  int iflag = 0; //indent
+  int bflag = 0; //block
+  int rflag = 0; //remove, implies -i
+  char *nvalue = NULL; // tab length
+  int index;
+  int c;
+
+  opterr = 0;
+  while ((c = getopt (argc, argv, "abc:")) != -1)
+    switch (c)
+      {
+      case 'i':
+        iflag = 1;
+        break;
+      case 'b':
+        bflag = 1;
+        break;
+      case 'n':
+        nvalue = optarg;
+        break;
+      case '?':
+        if (optopt == 'n')
+          fprintf (stderr, "Option -%n requires an argument.\n", optopt);
+        else if (isprint (optopt))
+          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        else
+          fprintf (stderr,
+                   "Unknown option character `\\x%x'.\n",
+                   optopt);
+        return 1;
+      default:
+        abort ();
+      }
+  //printf ("aflag = %d, bflag = %d, cvalue = %s\n",
+  //        aflag, bflag, cvalue);
+
+  //for (index = optind; index < argc; index++)
+  //  printf ("Non-option argument %s\n", argv[index]);
+
+  python_specific_subscribe(2,scan_callback);
+  //python_main_init(argc,argv);
+  python_main_init(argc-optind+1,&argv[optind-1]);
+
+  //std::cerr << "ready";
+  exit (0);
+
+}
