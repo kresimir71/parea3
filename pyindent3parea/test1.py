4b216a425f77cbe9c6f5ac105dd351377f8a7811
kresimir71 <kresjemir@kresjemir-PC.(none)>
2015-06-19 14:12:26 +0200

    working on indent

---
+++ pyindent3parea/test1.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python2.7
+
+class foo1(): #comment
+
+    def __init__(self, server, conn, addr): #{
+        self.__bar1 = socket.getfqdn()
+        try: #{{
+            self.__foo3 = bar3() #}}
+        except socket.error, err:#}
+            pass:#comment
+    def foo2(self):#{comment
+        line = EMPTYSTRING.join(self.__line)
+        print >> DEBUGSTREAM, 'Data:', repr(line)
+        self.__line = []
+        if self.__state == self.COMMAND:#{{{{#
+            if not line: #{comment
+                self.push('500 Error: bad syntax')
+                return
+            method = None #}}}}}}}}#
